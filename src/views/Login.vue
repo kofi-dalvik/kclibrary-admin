016ebccd6ad27e9a531672e01782ccf4da47ce5a
Dawuda Ahmed <kofidahmed@gmail.com>
2019-03-01 23:42:54 +0000

    added ui for login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,26 @@
 <template>
-<div>Login</div>
+<div class="login">
+    <div class="logo d-flex flex-column justify-content-end align-items-center">
+        <img src="https://source.unsplash.com/random">
+        <p>ADMIN</p>
+    </div>
+
+    <div class="form">
+        <div class="form-group">
+            <input type="text" placeholder="username" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <input type="password" placeholder="password" class="form-control">
+        </div>
+
+        <div class="form-group">
+        <button class="btn-block btn btn-default">LOGIN</button>
+        <a href="javascript:void(0)">Forgot Password?</a>
+        </div>
+    </div>
+
+</div>
 </template>
 
 <script>
