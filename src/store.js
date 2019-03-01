4b978bc76507340e2b6e799d660e8cd132017ed0
Dawuda Ahmed <kofidahmed@gmail.com>
2019-03-01 23:51:47 +0000

    added getters, login method and user object to state

--- src/store.js
+++ src/store.js
@@ -1,16 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: {}
+  },
+
+  getters: {
 
   },
+
   mutations: {
 
   },
-  actions: {
 
+  actions: {
+    login({commit}, userPayload) {
+      //make request to server
+    }
   }
 })
