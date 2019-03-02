62c75af273ccfa54fcc5111a650aff4a08ff1e95
Dawuda Ahmed <kofidahmed@gmail.com>
2019-03-02 03:47:53 +0000

    imported app-select  component

--- src/plugins.js
+++ src/plugins.js
@@ -1,8 +1,9 @@
 import './assets/style.scss';
 
+import AppSelect from './components/app-select.vue';
 
 export default {
     install(Vue) {
-
+        Vue.component('app-select', AppSelect);
     }
 }
