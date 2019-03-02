765aa577c28ac225262b6dcbf12ec1133072ffec
Dawuda Ahmed <kofidahmed@gmail.com>
2019-03-02 03:48:17 +0000

    refactored the routes in here

--- src/router.js
+++ src/router.js
@@ -1,47 +1,68 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from './store';
 
 Vue.use(Router)
 
-export default new Router({
+const routes = [{
+    path: '/',
+    name: 'login',
+    meta: {
+      title: 'Login'
+    },
+    component: () => import('./views/Login.vue')
+  },
+  {
+    name: 'auth',
+    path: '/auth',
+    meta: {
+      auth: true
+    },
+    component: () => import('./components/app-master.vue'),
+    children: [{
+        name: 'home',
+        path: 'home',
+        meta: {
+          title: 'KOTEI CONGREGATION'
+        },
+        component: () => import('./views/Home.vue')
+      },
+      {
+        name: 'edit-home',
+        path: 'edit-home',
+        meta: {
+          title: 'KOTEI CONGREGATION'
+        },
+        component: () => import('./views/EditHome.vue')
+      },
+      {
+        name: 'publishers',
+        path: 'publishers',
+        meta: {
+          title: 'PUBLISHERS'
+        },
+        component: () => import('./views/Publishers.vue')
+      },
+      {
+        name: 'settings',
+        path: 'settings',
+        meta: {
+          title: 'SETTINGS'
+        },
+        component: () => import('./views/Settings.vue')
+      }
+    ]
+  }
+]
+
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'login',
-      meta: {title: 'Login'},
-      component: () => import('./views/Login.vue')
-    },
-    {
-      name: 'auth',
-      path: '/auth',
-      meta: { auth: true },
-      component: () => import('./components/app-master.vue'),
-      children: [
-        {
-          name: 'home',
-          path: 'home',
-          meta: {
-            title: 'KOTEI CONGREGATION'
-          },
-          component: () => import('./views/Home.vue')
-        },
-        {
-          name: 'edit-home',
-          path: 'edit-home',
-          meta: {
-            title: 'KOTEI CONGREGATION'
-          },
-          component: () => import('./views/EditHome.vue')
-        },
-        {
-          name: 'publishers',
-          path: 'publishers',
-          meta: { title: 'PUBLISHERS'},
-          component: () => import('./views/Publishers.vue')
-        }
-      ]
-    }
-  ]
+  routes
+});
+
+router.afterEach((to, from) => {
+  store.dispatch('closeNav');
 })
+
+export default router;
